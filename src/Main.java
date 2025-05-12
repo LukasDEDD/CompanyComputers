886f7dde3e970f27e32bc4e270d6ca3cf0443089
LukasDD82 <lukas.simek.de@gmail.com>
2025-05-12 23:38:35 +0200

    Getter and Setter + 2xConstructor

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.math.BigDecimal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Computers direktor =
+                new Computers ("Dell",250.5, false, new BigDecimal("475.80"),true );
 
     }
 }
