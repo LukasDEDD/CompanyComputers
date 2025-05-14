f9f5bcd7edfae67eb56befc3618aa76f587ec332
LukasDD82 <lukas.simek.de@gmail.com>
2025-05-14 20:27:15 +0200

    Getter and Setter + 2xConstructor+List

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
 
         Computers direktor =
                 new Computers ("Dell",250.5, false, new BigDecimal("475.80"),true );
-    }
+
         Computers manager =
                 new Computers (250, true);
-}
 
+    List <Computers> notebooks = new ArrayList<>();
+        notebooks.add(direktor);
+        notebooks.add(manager);
+        notebooks.size();
+
+        System.out.println(notebooks.get(1));
+
+        }
+    }
